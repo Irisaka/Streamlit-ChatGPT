af4a322ffc3f626b67ecfafc74c1d1ded53d5386
Irisaka <107071603@qq.com>
2023-02-03 15:24:53 +0800

    true_chat

--- chatgptbot.py
+++ chatgptbot.py
@@ -1,67 +1,9 @@
 import streamlit as st
 from streamlit_chat import message
+from streamlit_elements import elements, mui, lazy, sync, event
 from revChatGPT.Official import Chatbot
 
 def main():
-    def get_input(prompt):
-        """
-        Multi-line input function
-        """
-        # Display the prompt
-        print(prompt, end="")
-
-        # Initialize an empty list to store the input lines
-        lines = []
-
-        # Read lines of input until the user enters an empty line
-        while True:
-            line = input()
-            if line == "":
-                break
-            lines.append(line)
-
-        # Join the lines, separated by newlines, and store the result
-        user_input = "\n".join(lines)
-
-        # Return the input
-        return user_input
-
-    def chatbot_commands(cmd: str) -> bool:
-        """
-        Handle chatbot commands
-        """
-        if cmd == "!help":
-            print(
-                """
-            !help - Display this message
-            !rollback - Rollback chat history
-            !reset - Reset chat history
-            !exit - Quit chat
-            """
-            )
-        elif cmd == "!exit":
-            exit()
-        elif cmd == "!rollback":
-            chatbot.rollback(1)
-        elif cmd == "!reset":
-            chatbot.reset()
-        else:
-            return False
-        return True
-
-    # import argparse
-
-    # # Get API key from command line
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "--api_key",
-    #     type=str,
-    #     required=True,
-    #     help="OpenAI API key",
-    # )
-    # args = parser.parse_args()
-    # # Initialize chatbot
-    # chatbot = Chatbot(api_key=args.api_key)
 
     st.set_page_config(
         page_title="",
@@ -74,40 +16,41 @@ def main():
     if 'message_history' not in st.session_state:
         st.session_state['message_history'] = []
 
+    if 'chatbot' not in st.session_state:
+        st.session_state.chatbot = Chatbot(api_key=st.secrets["API_KEY"])
+
+    # if 'user_input' not in st.session_state:
+    #     st.session_state.user_input = None
+
+    # if st.session_state.user_input is not None:
+    #     text = st.session_state.user_input.target.value
+    # else:
+    #     text = "你好"
+
     for index, message_ in enumerate(st.session_state['message_history']):
         message(message_[0], is_user=message_[1], key=index)   # display all the previous message
 
     placeholder = st.empty()  # placeholder for latest message
     placeholder2 = st.empty()
 
-    with st.form("my_form"):
-        input_ = st.text_input("you:")
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            user_input = input_
+    # with elements("user_input_submit"):
+    #     mui.TextField(label="you:", onChange=lazy(sync("user_input")))
+    #     mui.Button("Enter", onClick=sync())
 
-            st.session_state['message_history'].append([user_input, 1])
+    with st.form("user_input_form", clear_on_submit=True):
+        text = st.text_input('you:')
+        submitted = st.form_submit_button("Enter")
+        if submitted:
+            st.session_state['message_history'].append([text, 1])
 
             with placeholder.container():
                 message(st.session_state['message_history'][-1][0], is_user=st.session_state['message_history'][-1][1]) # display the latest message
 
-            # chatbot = Chatbot(api_key=st.secrets["API_KEY"])
-            # # Start chat
-            # while True:
-            #     # PROMPT = get_input("\nUser:\n")
-            #     PROMPT = 
-            #     if PROMPT.startswith("!"):
-            #         if chatbot_commands(PROMPT):
-            #             continue
-            #     response = chatbot.ask(PROMPT)
-            #     print("ChatGPT: " + response["choices"][0]["text"])
-
-            chatbot = Chatbot(api_key=st.secrets["API_KEY"])
-            PROMPT =  "\nUser:\n" + user_input
-            # if PROMPT.startswith("!"):
-            #     if chatbot_commands(PROMPT):
-            #         continue
+            chatbot = st.session_state.chatbot
+            PROMPT =  "\nUser:\n" + text
             response = chatbot.ask(PROMPT)
+            st.session_state.chatbot = chatbot
+
             print("ChatGPT: " + response["choices"][0]["text"])
             st.session_state['message_history'].append([response["choices"][0]["text"], 0])
             with placeholder2.container():
